e85cc19de6a6a83796fbcbbe35ce5dfe436aab2f
Paulo <paulozamora2000@gmail.com>
2023-04-09 14:11:17 -0600

    leader ff to format file

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -12,8 +12,12 @@ null_ls.setup({
         --null_ls.builtins.completion.spell,
     },
     on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then 
+        if client.server_capabilities.document_formatting then 
             vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
         end
     end
 })
+
+vim.keymap.set("n", "<leader>ff", function()
+    vim.lsp.buf.formatting_seq_sync()
+end)
