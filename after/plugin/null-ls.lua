a75007ea4d5fe476c02e6423270149a5bcf16604
Paulo <paulozamora2000@gmail.com>
2023-04-24 17:36:42 -0600

    Added prettierd for formatting, re-enabled format on save

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -3,21 +3,21 @@ local null_ls = require("null-ls")
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettier.with({
-            extra_args = {"--tab-width", "4"}
-        }),
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.codespell.with({filetypes = {'markdown'}}),
-        --null_ls.builtins.diagnostics.eslint
-        --null_ls.builtins.completion.spell,
-    },
-    on_attach = function()
-        --vim.api.nvim_create_autocmd("BufWritePost", {
-        --    callback = function()
-        --        vim.lsp.buf.format()
-        --    end,
-        --})
-    end,
+            null_ls.builtins.formatting.prettierd,
+            null_ls.builtins.code_actions.eslint,
+            null_ls.builtins.formatting.black,
+            null_ls.builtins.diagnostics.codespell.with({filetypes = {'markdown'}}),
+            --null_ls.builtins.completion.spell,
+        },
+    on_attach = function(client)
+        if client.server_capabilities.documentFormattingProvider then 
+            vim.api.nvim_create_autocmd("BufWritePost", {
+                callback = function()
+                    vim.lsp.buf.format ()
+                end
+            })
+        end
+    end
 })
 
 vim.keymap.set("n", "<leader>ff", function()
