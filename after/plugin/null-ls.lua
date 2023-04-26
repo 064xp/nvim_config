f42452197636040fcef9b2bb5f1679604df7d530
Paulo <paulozamora2000@gmail.com>
2023-04-26 13:25:54 -0600

    Configure prettierd tabwidth, telescope view document symbol map

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,19 +1,23 @@
 local null_ls = require("null-ls")
 
+local sources = {
+    null_ls.builtins.formatting.prettierd.with({
+        extra_args = {"--tab-width", "4"}
+    }),
+    null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.codespell.with({filetypes = {'markdown'}}),
+    --null_ls.builtins.completion.spell,
+}
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 null_ls.setup({
-    sources = {
-            null_ls.builtins.formatting.prettierd,
-            null_ls.builtins.code_actions.eslint,
-            null_ls.builtins.formatting.black,
-            null_ls.builtins.diagnostics.codespell.with({filetypes = {'markdown'}}),
-            --null_ls.builtins.completion.spell,
-        },
+    sources = sources,
     on_attach = function(client)
         if client.server_capabilities.documentFormattingProvider then 
-            vim.api.nvim_create_autocmd("BufWritePost", {
+            vim.api.nvim_create_autocmd("BufWritePre", {
                 callback = function()
-                    vim.lsp.buf.format ()
+                    vim.lsp.buf.format()
                 end
             })
         end
