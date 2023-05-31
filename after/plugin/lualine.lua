1c60507f4587938d8821f640fa7ec7daa962389d
Paulo <paulozamora2000@gmail.com>
2023-05-30 23:09:29 -0600

    Added filepath to lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,10 +1,16 @@
 require('lualine').setup({
     options = { theme = "horizon" },
     sections = {
+        lualine_c = {
+            {
+                'filename',
+                path = 1,
+            },
+        },
         lualine_a = {
             {
                 'buffers',
-            }
+            },
         }
     }
 })
